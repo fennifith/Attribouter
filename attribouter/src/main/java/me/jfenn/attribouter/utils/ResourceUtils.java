ff3f570d8532c705ff2b68b8068d47ba5648a6ca
James Fenn <18jafenn90@gmail.com>
2018-03-11 19:22:15 -0400

    simplified ResourceUtils

--- attribouter/src/main/java/me/jfenn/attribouter/utils/ResourceUtils.java
+++ attribouter/src/main/java/me/jfenn/attribouter/utils/ResourceUtils.java
@@ -10,33 +10,37 @@ public class ResourceUtils {
         if (identifier != null && identifier.startsWith("^"))
             identifier = identifier.substring(1);
 
+        Integer resource = getResourceInt(context, identifier);
+        if (identifier != null && identifier.startsWith("@"))
+            identifier = null;
+
+        return resource != null ? context.getString(resource) : identifier;
+    }
+
+    @Nullable
+    public static Integer getResourceInt(Context context, @Nullable String identifier) {
+        if (identifier != null && identifier.startsWith("^"))
+            identifier = identifier.substring(1);
+
         if (identifier != null && identifier.startsWith("@")) {
             identifier = identifier.substring(1);
             if (identifier.contains("/")) {
                 String[] identifiers = identifier.split("/");
                 if (identifiers[0].length() > 0 && identifiers[1].length() > 0) {
-                    int stringRes = context.getResources().getIdentifier(identifiers[1], identifiers[0], context.getPackageName());
-                    return stringRes == 0 ? null : context.getString(stringRes);
+                    int res = context.getResources().getIdentifier(identifiers[1], identifiers[0], context.getPackageName());
+                    return res == 0 ? null : res;
                 }
             } else {
                 try {
-                    return context.getString(Integer.parseInt(identifier));
+                    return Integer.parseInt(identifier);
                 } catch (NumberFormatException ignored) {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            return null;
-        } else return identifier;
-    }
+        }
 
-    @Nullable
-    public static Integer getResourceInt(Context context, @Nullable String identifier) {
-        if (identifier != null && identifier.startsWith("@") && identifier.contains("/")) {
-            identifier = identifier.substring(1);
-            int res = context.getResources().getIdentifier(context.getApplicationInfo().packageName + ":" + identifier, null, null);
-            return res == 0 ? null : res;
-        } else return null;
+        return null;
     }
 
 }
