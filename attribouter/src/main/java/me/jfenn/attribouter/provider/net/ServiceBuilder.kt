ef5a8b3ab67eb9dc0a8098442f4df56d102ae9d4
James Fenn <me@jfenn.me>
2020-04-05 00:02:05 -0400

    add proguard rules - #66

--- attribouter/src/main/java/me/jfenn/attribouter/provider/net/ServiceBuilder.kt
+++ attribouter/src/main/java/me/jfenn/attribouter/provider/net/ServiceBuilder.kt
@@ -4,7 +4,6 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 interface ServiceBuilder<T: RequestProvider> {
@@ -34,7 +33,6 @@ interface ServiceBuilder<T: RequestProvider> {
 
     fun retrofit(): Retrofit.Builder {
         return Retrofit.Builder()
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(okhttp().build())
     }
